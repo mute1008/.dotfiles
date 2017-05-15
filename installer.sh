66fa73ecb166d10526528a19fdbe968a5529e827
mute1997 <mute1008@gmail.com>
2017-05-16 05:23:53 +0900

    add gdb settings

--- installer.sh
+++ installer.sh
@@ -2,3 +2,9 @@
 mkdir -p ~/.config/nvim
 cp ./vim/nvim/* ~/.config/nvim/
 cp ./vim/.vimrc ~/.vimrc
+
+# GDB
+if [ `uname` == 'Linux' ]; then
+  wget -q -O- https://github.com/hugsy/gef/raw/master/gef.sh | sh
+  echo "disassembly-flavor intel" >> $HOME/.gdbinit
+fi
