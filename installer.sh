52ce6d2c99fe9c1f487fc7a8e6f471c41003e282
mute1997 <mute1008@gmail.com>
2017-05-22 19:11:52 +0900

    add linux setting

--- installer.sh
+++ installer.sh
@@ -6,8 +6,9 @@ cp ./.tmux.conf ~/.tmux.conf
 cp ./.bash_profile ~/.bash_profile
 cp ./.tmux.conf ~/.tmux.conf
 
-# GDB
 if [ `uname` == 'Linux' ]; then
   wget -q -O- https://github.com/hugsy/gef/raw/master/gef.sh | sh
   echo "disassembly-flavor intel" >> $HOME/.gdbinit
+
+  mv ~/.bash_profile ~/.bashrc
 fi
