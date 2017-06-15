4ccbae223f9b06694cd13e900597dc9eccddcfe3
mute1997 <mute1008@gmail.com>
2017-06-15 12:09:36 +0900

    add vimdiff prompt true

--- installer.sh
+++ installer.sh
@@ -7,9 +7,9 @@ cp ./.bash_profile ~/.bash_profile
 cp ./.tmux.conf ~/.tmux.conf
 
 git config --global diff.tool vimdiff
-git config --global difftool.prompt false
+git config --global difftool.prompt true
 git config --global merge.tool vimdiff
-git config --global mergetool.prompt false
+git config --global mergetool.prompt true
 
 if [ `uname` == 'Linux' ]; then
   wget -q -O- https://github.com/hugsy/gef/raw/master/gef.sh | sh
