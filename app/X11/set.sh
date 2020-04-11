8923a08911f5cc808a1ebe69cb9a8e29fae815c2
mute1997 <mute1008@gmail.com>
2020-04-11 21:33:48 +0900

    remove X11

--- app/X11/set.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source lib/symlink.sh
-source lib/os.sh
-
-if [ $(get_os) = "Linux" ]; then
-  symlink ./app/X11/files/Xdefaults ~/.Xdefaults
-  symlink ./app/X11/files/xprofile ~/.xprofile
-fi
