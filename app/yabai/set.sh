d470280715b7fbee4de0d6e45873642c3faeb9a9
mute1997 <mute1008@gmail.com>
2020-04-17 22:14:23 +0900

    add yabai settings

--- app/yabai/set.sh
+++ app/yabai/set.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source lib/symlink.sh
+source lib/os.sh
 
-symlink ./app/yabai/files/yabairc ~/.yabairc
-symlink ./app/yabai/files/skhdrc ~/.skhdrc
+if [ $(get_os) = 'Darwin' ]; then
+  symlink ./app/yabai/files/yabairc ~/.yabairc;
+  symlink ./app/yabai/files/skhdrc ~/.skhdrc;
+fi
