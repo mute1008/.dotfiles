8df546b413a57a6e247272205404f3fe198e7b6b
mute1997 <mute1008@gmail.com>
2020-07-03 22:30:23 +0900

    add fpull

--- app/git/set.sh
+++ app/git/set.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+source lib/bin.sh
+
 git config --global user.name mute1997
 git config --global user.email mute1008@gmail.com
 git config --global core.excludesfile $(pwd)/app/git/files/gitignore
+
+mkdir -p ~/.userbin/
+safe_ln $(pwd)/app/git/files/git-fpull ~/.userbin/git-fpull
