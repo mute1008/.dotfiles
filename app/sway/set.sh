d72d69702364bb68701da7d525f87bf848deb8cd
mute1997 <mute1008@gmail.com>
2020-10-01 19:29:26 +0900

    add waybar setting

--- app/sway/set.sh
+++ app/sway/set.sh
@@ -4,4 +4,6 @@ source lib/bin.sh
 
 if [ $(uname) = 'Linux' ]; then
   safe_ln $(pwd)/app/sway/files/config ~/.config/sway/config;
+  safe_ln $(pwd)/app/sway/files/style.css ~/.config/waybar/style.css;
+  safe_ln $(pwd)/app/sway/files/waybar_config ~/.config/waybar/config;
 fi
