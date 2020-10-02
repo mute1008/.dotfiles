70549b9a5709b8ec2689d694b77d85757de749b3
mute1997 <mute1008@gmail.com>
2020-10-02 13:01:21 +0900

    split sway setting

--- app/sway/set.sh
+++ app/sway/set.sh
@@ -4,6 +4,8 @@ source lib/bin.sh
 
 if [ $(uname) = 'Linux' ]; then
   safe_ln $(pwd)/app/sway/files/config ~/.config/sway/config;
+  safe_ln $(pwd)/app/sway/files/laptop.conf ~/.config/sway/laptop.conf;
+  safe_ln $(pwd)/app/sway/files/desktop.conf ~/.config/sway/desktop.conf;
   safe_ln $(pwd)/app/sway/files/style.css ~/.config/waybar/style.css;
   safe_ln $(pwd)/app/sway/files/waybar_config ~/.config/waybar/config;
 fi
