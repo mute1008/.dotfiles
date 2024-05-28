86b991b77d78f0f2fd7d795a0732005f03530681
mute1997 <mute1008@gmail.com>
2024-05-28 20:48:34 +0900

    renew ssh key

--- source/export.sh
+++ source/export.sh
@@ -26,3 +26,5 @@ export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
 export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
 
+# brew
+export PATH=$PATH:/opt/homebrew/bin/
