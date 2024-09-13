dea516a1522ca1ab0601840817188b6abaf2330d
mute1997 <mute1008@gmail.com>
2024-09-13 16:51:09 +0900

    add extract command

--- source/alias.sh
+++ source/alias.sh
@@ -8,3 +8,25 @@ if [[ `uname` == 'Darwin' ]]; then
   alias find=gfind
   alias xargs=gxargs
 fi
+
+ext() {
+  if [ -f "$1" ] ; then
+    dir_name="${1%.*}"
+    mkdir -p "$dir_name" && cd "$dir_name"
+    case "$1" in
+      *.tar.bz2|*.tbz2) tar xjf "../$1" ;;
+      *.tar.gz|*.tgz) tar xzf "../$1" ;;
+      *.bz2) bunzip2 "../$1" ;;
+      *.rar) unrar x "../$1" ;;
+      *.gz) gunzip "../$1" ;;
+      *.tar) tar xf "../$1" ;;
+      *.zip) unzip "../$1" ;;
+      *.Z) uncompress "../$1" ;;
+      *.7z) 7z x "../$1" ;;
+      *) echo "'$1' can't be extracted" && cd .. && rm -r "$dir_name" ;;
+    esac
+    cd ..
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
