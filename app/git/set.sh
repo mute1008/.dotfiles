e9abe7d361c4f975ad11955664e762e96983722e
mute1997 <mute1008@gmail.com>
2025-05-18 16:16:06 +0900

    add gitconfig

--- app/git/set.sh
+++ app/git/set.sh
@@ -4,4 +4,5 @@ source source/lib.sh
 
 git config --global user.name mute1997
 git config --global user.email mute1008@gmail.com
+git config pull.rebase true
 safe_ln $(pwd)/app/git/files/gitconfig ~/.gitconfig
